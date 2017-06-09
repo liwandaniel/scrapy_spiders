3f797311c3845f03bf2840cb739ecf617963dd38
Daniel <454644692@qq.com>
2017-06-09 18:17:14 +0800

    Article_spider of jobbole

---
+++ main.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+
+from scrapy.cmdline import execute
+import sys
+import os
+
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))  #获取main文件的目录的父目录
+execute(["scrapy", "crawl", "jobbole"])
